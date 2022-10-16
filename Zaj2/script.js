08d57c5c160bb33c298a55db32d1718a612f7f6b
MaciejJar <maciej.jarmula2000@gmail.com>
2022-10-16 14:22:14 +0200

    Zaj2 - Slider

---
+++ Zaj2/script.js
@@ -0,0 +1,7 @@
+const divSlide = document.querySelector('.slide')
+const divImgSlide = document.querySelectorAll('.imgSlide img')
+const buttonBack = document.querySelector('#back')
+const buttonNext = document.querySelector('#next')
+let number = 1
+
+
