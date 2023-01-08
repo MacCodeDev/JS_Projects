e5b24392eeb86f31f7773ae2cbb23873524e78a4
MaciejJar <maciej.jarmula2000@gmail.com>
2023-01-08 15:07:05 +0100

    WeatherApp create url

--- Zaj7/app.js
+++ Zaj7/app.js
@@ -1,1 +1,7 @@
 const cityName = document.querySelector('#cityName')
+const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q='
+const apiKey = '&appid=ccaf921bf5268d1c483f1d5a08148ea6&units=metric'
+
+const weatherInfo = () =>{
+    const fullUrl = apiLink + 'Krakow' + apiKey
+}
