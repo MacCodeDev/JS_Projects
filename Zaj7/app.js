8ac149018f709cab92fdd36de4864e371a7c942d
MaciejJar <maciej.jarmula2000@gmail.com>
2023-01-20 00:20:14 +0100

    Weather app add and delete city

--- Zaj7/app.js
+++ Zaj7/app.js
@@ -6,29 +6,31 @@ const photo = document.querySelector('.photo')
 const cityButton = document.querySelector('#cityButton')
 const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q='
 const apiKey = '&appid=ccaf921bf5268d1c483f1d5a08148ea6&units=metric'
+let ulList = document.querySelector('.list ul')
+let newWeather, header, img, deleteBtn
 
-const weatherInfo = () =>{
-    const fullUrl = apiLink + 'Krakow' + apiKey
-}
+
 
 const getInfo = () => {
-    const city = inputCity.value || 'Warszawa'
-    const fullUrl = apiLink + city + apiKey
-    fetchRequest(fullUrl)
+    if (ulList.childElementCount < 10) {
+        const city = inputCity.value || 'Warszawa'
+        const fullUrl = apiLink + city + apiKey
+        fetchRequest(fullUrl)
+    }
+    else{
+        alert('You can\'t have more than ten cities')
+    }
 }
-getInfo()
 function fetchRequest( url ) {
     fetch(url)
         .then(function(resp) { return resp.json() })
         .then(function(data) {
             const temperatureData = data.main.temp
             const weatherStatus = data.weather[0]
-            console.log(data.weather[0])
-            temperature.textContent =  Math.floor(temperatureData) + '℃'
-            humidity.textContent = data.main.humidity + '%'
-            cityName.textContent = data.name
-            photo.src = 'http://openweathermap.org/img/wn/' + weatherStatus.icon + '@2x.png'
+            const tempPhoto = 'http://openweathermap.org/img/wn/' + weatherStatus.icon + '@2x.png'
+            addNewCity(data.name,Math.floor(temperatureData) + '℃',data.main.humidity + '%',tempPhoto)
             inputCity.value = ""
+            console.log()
         })
         .catch(function() {
             alert('Not found')
@@ -39,5 +41,48 @@ const checkKey = (x) =>{
         getInfo()
     }
 }
+
+const addNewCity = (city,tempe,humid,imgSrc) => {
+        newWeather = document.createElement('li')
+        header = document.createElement('h2')
+        newWeather.append(header)
+        img = document.createElement('img')
+        img.src = imgSrc
+        newWeather.append(img)
+        header.textContent = city
+        const dataDiv  = document.createElement('div')
+        dataDiv.classList.add('Weather-section')
+        newWeather.append(dataDiv)
+        const dataTemp  = document.createElement('p')
+        dataTemp.classList.add('temperature')
+        dataTemp.textContent = tempe
+        dataDiv.append(dataTemp)
+        const dataHum  = document.createElement('p')
+        dataHum.classList.add('humidity')
+        dataHum.textContent = humid
+        dataDiv.append(dataHum)
+        deleteBtn = document.createElement('button')
+        deleteBtn.classList.add('delete')
+        deleteBtn.textContent = 'test'
+        newWeather.append(deleteBtn)
+        ulList.append(newWeather)
+
+}
+
+const checkClick = e => {
+    if(e.target.matches('.delete')){
+        deleteCity(e)
+    }
+}
+
+const deleteCity = (event) =>{
+    const allCity = ulList.querySelectorAll('li')
+    event.target.closest('li').remove()
+    if(allCity.length === 0){
+        alert('City list is empty!')
+    }
+}
+
+ulList.addEventListener('click',checkClick)
 inputCity.addEventListener('keyup', checkKey)
 cityButton.addEventListener('click', getInfo)
